e6ac7367df1b99524e0a77cf9bf364bec66d5086
Eric <gs@aula11-12>
2021-04-22 16:37:16 +0200

    Add Form Game And Class Error

---
+++ js/errorGame.js
@@ -0,0 +1,23 @@
+export default class ErrorGame {
+  constructor(codiError, miss){
+    switch (codiError) {
+      case 1:
+
+        break;
+      case 2:
+
+        break;
+      case 3:
+
+        break;
+      case 4:
+
+        break;
+      case 5:
+
+        break;
+      default:
+
+    }
+  }
+}
