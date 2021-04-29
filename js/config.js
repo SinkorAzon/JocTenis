8484bcbbb270599f1fffc46c3295cec177e73445
Eric <gs@aula11-12>
2021-04-29 18:19:22 +0200

    Add new Cointainer for Game Config

---
+++ js/config.js
@@ -0,0 +1,19 @@
+import  { } from '/js/sketch.js';
+import  { } from '/js/status.js';
+
+const s = ( sketch ) => {
+
+  sketch.preload = function(){
+
+  }
+
+  sketch.setup = function(){
+    sketch.createCanvas(300, 300);
+  }
+
+  sketch.draw = function(){
+
+  }
+}
+
+var myp5 = new p5(s, 'myContainerConfig');
